88acd152c01898c2f20f933a28440019ae3b833a
Rune Harlyk <rune@harlyk.dk>
2026-02-19 19:01:05 +0100

    Finetune job install all dependencies

--- jobs/finetune_smolvla.sh
+++ jobs/finetune_smolvla.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # ---------------- LSF directives ----------------
-#BSUB -J smolvla-libero90-finetune
+#BSUB -J smolvla-libero-finetune
 #BSUB -q gpua100
 #BSUB -W 24:00
 #BSUB -n 8
@@ -25,6 +25,9 @@ export WANDB_DIR=/work3/s234814/.cache/wandb
 export WANDB_CACHE_DIR=/work3/s234814/.cache/wandb
 export UV_CACHE_DIR=/work3/s234814/.cache/uv
 export UV_PROJECT_ENVIRONMENT=/work3/s234814/.venvs/vla-robotics
+export MUJOCO_GL=egl
+export PYOPENGL_PLATFORM=egl
+export EGL_DEVICE_ID=0
 export PYTHONUNBUFFERED=1
 
 mkdir -p "$HF_HOME" "$WANDB_DIR" "$UV_CACHE_DIR" "$UV_PROJECT_ENVIRONMENT" logs
@@ -33,18 +36,25 @@ module load cuda/12.2
 
 nvidia-smi
 
-uv sync
+uv sync --extra sim
 
 uv run lerobot-train \
-    --dataset.repo_id=lerobot/libero_90_image \
+    --dataset.repo_id=HuggingFaceVLA/libero \
     --policy.type=smolvla \
     --policy.pretrained_path=lerobot/smolvla_base \
+    --policy.load_vlm_weights=true \
+    --policy.push_to_hub=false \
+    --policy.device=cuda \
+    --policy.optimizer_lr=1e-4 \
+    --env.type=libero \
+    --env.task=libero_10 \
     --steps=50000 \
     --batch_size=64 \
-    --optimizer.lr=1e-4 \
-    --policy.device=cuda \
-    --output_dir=outputs/smolvla_libero90 \
+    --eval_freq=5000 \
+    --eval.n_episodes=10 \
+    --eval.batch_size=1 \
+    --output_dir=outputs/smolvla_libero \
     --save_checkpoint=true \
     --save_freq=5000 \
     --wandb.enable=true \
-    --wandb.project=vla-smolvla-libero90
+    --wandb.project=vla-smolvla-libero
