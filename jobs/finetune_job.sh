98c30a4e6940431fb3510b6e9cf53941554424e5
Rune Harlyk <rdh@edora.hq>
2026-02-19 10:13:42 +0100

    Updated finetuning job to train on libero-90

--- jobs/finetune_job.sh
+++ jobs/finetune_job.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # ---------------- LSF directives ----------------
-#BSUB -J smolvla-libero
-#BSUB -q gpul40s
+#BSUB -J smolvla-libero90-finetune
+#BSUB -q gpua100
 #BSUB -W 24:00
 #BSUB -n 8
 #BSUB -R "span[hosts=1]"
-#BSUB -R "rusage[mem=4GB]"
+#BSUB -R "rusage[mem=8GB]"
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -u s234814@dtu.dk
 #BSUB -B
@@ -17,11 +17,16 @@
 
 set -e
 
+cd "$LSB_SUBCWD"
+
+exec 2>&1
+
 export HF_HOME=/work3/s234814/.cache/huggingface
 export WANDB_DIR=/work3/s234814/.cache/wandb
 export WANDB_CACHE_DIR=/work3/s234814/.cache/wandb
 export UV_CACHE_DIR=/work3/s234814/.cache/uv
 export UV_PROJECT_ENVIRONMENT=/work3/s234814/.venvs/vla-robotics
+export PYTHONUNBUFFERED=1
 
 mkdir -p "$HF_HOME" "$WANDB_DIR" "$UV_CACHE_DIR" "$UV_PROJECT_ENVIRONMENT"
 
@@ -32,11 +37,11 @@ nvidia-smi
 uv sync
 
 uv run lerobot_train \
-    --dataset.repo_id=lerobot/libero_10_image \
+    --dataset.repo_id=lerobot/libero_90_image \
     --policy.type=smolvla \
     --policy.pretrained_path=lerobot/smolvla_base \
-    --steps=20000 \
+    --steps=50000 \
     --batch_size=64 \
     --lr=1e-4 \
     --device=cuda \
-    --wandb.project=vla-smolvla-libero
+    --wandb.project=vla-smolvla-libero90
