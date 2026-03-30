0fd0848d52a05dc29bf5995bc512fe691e8ac3fb
Rune Harlyk <rune@harlyk.dk>
2026-03-30 11:02:17 +0200

    Test larger batch

--- jobs/sparse_fpo_l40s.sh
+++ jobs/sparse_fpo_l40s.sh
@@ -3,7 +3,7 @@
 # ---------------- LSF directives ----------------
 #BSUB -J sparse_fpo_l40s
 #BSUB -q gpul40s
-#BSUB -W 16:00
+#BSUB -W 24:00
 #BSUB -n 16
 #BSUB -R "span[hosts=1]"
 #BSUB -R "rusage[mem=4GB]"
@@ -23,7 +23,7 @@ uv run python scripts/train_srpo.py \
   --simulator libero \
   --suite spatial \
   --libero-suite spatial \
-  --task-ids all \
+  --task-ids 2 \
   --mode sparse_rl \
   --update-method fpo \
   --advantage-mode leave_one_out \
@@ -31,14 +31,14 @@ uv run python scripts/train_srpo.py \
   --lr 3e-06 \
   --max-grad-norm 10.0 \
   --iterations 35 \
-  --trajs-per-task 32 \
+  --trajs-per-task 64 \
   --num-rollout-envs 8 \
   --fm-batch-size 64 \
   --ppo-epochs 1 \
   --clip-epsilon 0.05 \
   --clip-epsilon-high 0.08 \
   --num-fm-noise-samples 4 \
-  --fpo-negative-adv-scale 0.75 \
+  --fpo-negative-adv-scale 1 \
   --kl-coeff 0.01 \
   --adv-eps 1e-8 \
   --adv-skip-threshold 1e-6 \
@@ -46,5 +46,5 @@ uv run python scripts/train_srpo.py \
   --eval-episodes 50 \
   --max-steps 220 \
   --gradient-checkpointing \
-  --wandb-name "v5-t_all-fpo-neg-adv-0.75" \
+  --wandb-name "v11-t-2-larger-batch" \
   --wandb
