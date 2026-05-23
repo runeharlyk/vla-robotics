13e6824cb820a9bdc46d01546533dfffa72c4b0b
CAN103 <canickelsen@gmail.com>
2026-05-23 16:06:26 +0200

    fixes jobs

--- jobs/visual_cor_suc_s3.sh
+++ jobs/visual_cor_suc_s3.sh
@@ -8,19 +8,19 @@
 #BSUB -R "span[hosts=1]"
 #BSUB -R "rusage[mem=6GB]"
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -u s234809@dtu.dk
+#BSUB -u s234863@dtu.dk
 #BSUB -B
 #BSUB -N
-#BSUB -oo logs/visual_diagnostic_s3/%J.out
+#BSUB -oo logs/visual_cor_suc_s3/%J.out
 # -------------------------------------------------
 
 . jobs/_env.sh
 
-export LIBERO_PATH=/work3/s234809/libero
+export LIBERO_PATH=/work3/s234863/libero
 mkdir -p "$LIBERO_PATH"
-printf "Y\n/work3/s234814/libero\nY\n" | uv run python -c "import libero.libero; print('Libero configured')"
+printf "Y\n/work3/s234863/libero\nY\n" | uv run python -c "import libero.libero; print('Libero configured')"
 
 # Run your script
 uv run python -m visual_diagnostic.visual_sensitivity \
   --severity 3 \
-  --output-dir visual_diagnostic/outputs/s3
+  --output-dir /work3/s234863/visual_cor_suc_s3 \
--- jobs/visual_cor_suc_s5.sh
+++ jobs/visual_cor_suc_s5.sh
@@ -2,25 +2,25 @@
 
 # ---------------- LSF directives ----------------
 #BSUB -J visual_cor_suc_s5
-#BSUB -q gpua40
+#BSUB -q gpua100
 #BSUB -W 04:00
 #BSUB -n 8
 #BSUB -R "span[hosts=1]"
 #BSUB -R "rusage[mem=6GB]"
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -u s234809@dtu.dk
+#BSUB -u s234863@dtu.dk
 #BSUB -B
 #BSUB -N
-#BSUB -oo logs/visual_diagnostic_s5/%J.out
+#BSUB -oo logs/visual_cor_suc_s5/%J.out
 # -------------------------------------------------
 
 . jobs/_env.sh
 
-export LIBERO_PATH=/work3/s234809/libero
+export LIBERO_PATH=/work3/s234863/libero
 mkdir -p "$LIBERO_PATH"
-printf "Y\n/work3/s234814/libero\nY\n" | uv run python -c "import libero.libero; print('Libero configured')"
+printf "Y\n/work3/s234863/libero\nY\n" | uv run python -c "import libero.libero; print('Libero configured')"
 
 # Run your script
 uv run python -m visual_diagnostic.visual_sensitivity \
   --severity 5 \
-  --output-dir visual_diagnostic/outputs/s5
+  --output-dir /work3/s234863/visual_cor_suc_s5 \
