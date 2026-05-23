2f32547ec89042ed849646f8eb470010ab83d5e1
CAN103 <canickelsen@gmail.com>
2026-05-23 14:49:35 +0200

    Adds jobs for visual perturbations succes analysis
    Difficulty 1,3,5

---
+++ jobs/visual_cor_suc_s3.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# ---------------- LSF directives ----------------
+#BSUB -J visual_cor_suc_s3
+#BSUB -q gpua40
+#BSUB -W 04:00
+#BSUB -n 8
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=6GB]"
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -u s234809@dtu.dk
+#BSUB -B
+#BSUB -N
+#BSUB -oo logs/visual_diagnostic_s3/%J.out
+# -------------------------------------------------
+
+. jobs/_env.sh
+
+export LIBERO_PATH=/work3/s234809/libero
+mkdir -p "$LIBERO_PATH"
+printf "Y\n/work3/s234814/libero\nY\n" | uv run python -c "import libero.libero; print('Libero configured')"
+
+# Run your script
+uv run python -m visual_diagnostic.visual_sensitivity \
+  --severity 3 \
+  --output-dir visual_diagnostic/outputs/s3
